66dd9e20165cc6e0b651c351e1f05cb1a136f6b7
thumbimigwe <thumbimigwe@gmail.com>
2016-03-17 04:16:49 +0300

    django files

---
+++ django-user/bin/django-admin.py
@@ -0,0 +1,4 @@
+#!/home/thumbimigwe/Documents/aGithubThosh/magame/django-user/bin/python
+# EASY-INSTALL-SCRIPT: 'Django==1.9.4','django-admin.py'
+__requires__ = 'Django==1.9.4'
+__import__('pkg_resources').run_script('Django==1.9.4', 'django-admin.py')
