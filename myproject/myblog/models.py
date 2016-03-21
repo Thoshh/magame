2c1cc698f105696f1f9eb82680909352fe4fe235
thumbimigwe <thumbimigwe@gmail.com>
2016-03-22 00:01:46 +0300

    models - Body,Title & Tags

--- myproject/myblog/models.py
+++ myproject/myblog/models.py
@@ -3,3 +3,18 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Tag (models.Model):
+    name = models.CharField(max_length=255)
+    description = models.Charfield(max_legnth=255, null=True, default'')
+
+    def __str__(self):
+        return self.name
+
+class MyBlog(models.Model)
+    title = models.CharField(max_legnth=255)
+    body = models.CharField(max_legnth=40000)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self)
+        return self.title
