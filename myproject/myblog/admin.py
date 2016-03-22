56051f942d4d198a1cc7e3c7b14cc1888dd8cf90
thumbimigwe <thumbimigwe@gmail.com>
2016-03-22 04:26:42 +0300

    reverted2

--- myproject/myblog/admin.py
+++ myproject/myblog/admin.py
@@ -3,8 +3,10 @@
 
 from django.contrib import admin
 from django import forms
+from django import Post
 
 from myblog.models import MyBlog, Tag
 
 admin.site.register(MyBlog)
 admin.site.register(Tag)
+admin.site.register(Post)
