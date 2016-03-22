e675001413ba7e900992c4311bf3a2c6d79aa162
thumbimigwe <thumbimigwe@gmail.com>
2016-03-22 04:50:08 +0300

    Added Title, Body & tags fields in the models.py

--- myproject/myblog/models.py
+++ myproject/myblog/models.py
@@ -1,5 +1,20 @@
+# Create your models here.
 from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, null=True, default='')
+
+    def __str__(self):
+        return self.name
+
+
+class MyBlog(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.CharField(max_length=20000)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
