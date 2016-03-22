66f654e23816d9312f4bfc327ff4990b94031a9d
thumbimigwe <thumbimigwe@gmail.com>
2016-03-22 03:14:32 +0300

    modified the urls

--- myproject/myblog/admin.py
+++ myproject/myblog/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
-
+# Location myproject>myblog>admin.py
 # Register your models here.
+
+from django.contrib import admin
+from django import forms
+
+from myblog.models import MyBlog, Tag
+
+admin.site.register(MyBlog)
+admin.site.register(Tag)
