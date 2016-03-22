1b63610fe817d331425fd070ce109393be778c66
thumbimigwe <thumbimigwe@gmail.com>
2016-03-22 05:01:20 +0300

    edited the models.py

--- myproject/myblog/models.py
+++ myproject/myblog/models.py
@@ -1,8 +1,7 @@
-# Create your models here.
-from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
+# Create your models here.
 class Tag(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255, null=True, default='')
@@ -10,11 +9,15 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
-
 class MyBlog(models.Model):
     title = models.CharField(max_length=255)
     body = models.CharField(max_length=20000)
+    author = models.ForeignKey(User, null=True)
+    publishing_date = models.DateTimeField(auto_now_add=True)
     tags = models.ManyToManyField(Tag)
+    slug = models.SlugField()
 
     def __str__(self):
         return self.title
+
+
